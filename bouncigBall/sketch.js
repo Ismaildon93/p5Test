a383622af7572500412c0e679f2d7cc771aef8b9
Ismail Donà <ismaildon93@gmail.com>
2018-06-10 16:01:20 +0200

    added bouncing folder

--- bouncigBall/sketch.js
+++ bouncigBall/sketch.js
@@ -1,7 +1,25 @@
+var x;
+var speed = 3; // speed of the circle
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(600, 400);
+	x = 0;
 }
 
 function draw() {
+	background(0);
+
+	//create circle
+	noStroke();
+	fill(255);
+	ellipse(x, height / 2, 50);
+
+	if(x > width)
+		speed = -speed;
+
+	else if(x < 0)
+		speed = -speed;
+
+	x += speed;
 
 }
