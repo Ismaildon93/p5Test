1aeb47bb62937619713d33e26da998cd0c73f595
Ismail Donà <ismaildon93@gmail.com>
2018-07-02 15:31:53 +0200

    Update bouncingBall/sketch.js Fixed problem

--- bouncigBall/sketch.js
+++ bouncigBall/sketch.js
@@ -1,25 +1,59 @@
-var x;
-var speed = 3; // speed of the circle
+function Circle(xPos, yPos, diameter){
+	this.xPos = xPos; // the initial x position on the canvas
+	this.yPos = yPos; // the initial y position on the canvas
+	this.diameter = diameter; // the diameter of the circle
+	this.radius = this.diameter / 2; // the radius of the circle
+	this.xSpeed = 2; // the speed of the circle
+	this.color = new Color();
+
+	this.drawCircle = function (){
+
+		this.xPos += this.xSpeed;
+
+		noStroke();
+		fill(this.color.r, this.color.g, this.color.b);
+		ellipse(this.xPos, this.yPos, this.radius);
+}
+
+	this.changeColor = function(){
+		circle.color.setRandomColor();
+	}
+}
+
+function Color(){
+
+	this.r = 255;
+	this.g = 255;
+	this.b = 255;
+
+	//change randomly color
+	this.setRandomColor = function(){
+			this.r = parseInt(random(150,250));
+			this.g = parseInt(random(150,250));
+			this.b = parseInt(random(150,250));
+	}
+}
 
 function setup() {
 	createCanvas(600, 400);
-	x = 0;
+	circle = new Circle(0, height / 2, 100);
+
 }
 
 function draw() {
+
 	background(0);
 
-	//create circle
-	noStroke();
-	fill(255);
-	ellipse(x, height / 2, 50);
+	if(circle.xPos > width){
+		circle.xSpeed *= -1;
+		circle.changeColor();
+	}
 
-	if(x > width)
-		speed = -speed;
+	else if(circle.xPos < 0){
+		circle.xSpeed *= -1;
+		circle.changeColor();
+	}
 
-	else if(x < 0)
-		speed = -speed;
-
-	x += speed;
+	circle.drawCircle();
 
 }
